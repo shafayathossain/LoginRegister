5379e52edf8841acb640f3ce487beae13a562b21
shafayathossain <shafayathossainkhan@gmail.com>
2017-08-18 17:29:32 +0600

    Activity Setup done

--- app/src/main/java/com/example/shafayat/loginregister/MainActivity.java
+++ app/src/main/java/com/example/shafayat/loginregister/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.shafayat.loginregister;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText loginUserName;
+    EditText loginPassword;
+    Button loginButton;
+    TextView registerHere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginUserName = (EditText) findViewById(R.id.loginUserNameField);
+        loginPassword = (EditText) findViewById(R.id.loginPasswordField);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        registerHere = (TextView) findViewById(R.id.registerLink);
+
+        registerHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                MainActivity.this.startActivity(registerIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/shafayat/loginregister/RegisterActivity.java
+++ app/src/main/java/com/example/shafayat/loginregister/RegisterActivity.java
@@ -2,12 +2,27 @@ package com.example.shafayat.loginregister;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText registerName;
+    EditText registerUserName;
+    EditText registerEmail;
+    EditText RegisterPassword;
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        registerName = (EditText) findViewById(R.id.registerNameField);
+        registerUserName = (EditText) findViewById(R.id.registerUserNameField);
+        registerEmail = (EditText) findViewById(R.id.registerEmailField);
+        RegisterPassword= (EditText) findViewById(R.id.registerPasswordField);
+        registerButton = (Button) findViewById(R.id.registerButton);
+
     }
 }
